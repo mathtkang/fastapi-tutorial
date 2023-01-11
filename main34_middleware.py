11a350b957eba501b9ff5a0a3420b98799a49bde
[2기-1반] 강석영 <ksge1124@gmail.com>
2023-01-11 17:56:18 +0900

    middleware

--- main34_middleware.py
+++ main34_middleware.py
@@ -8,8 +8,10 @@ app = FastAPI()
 # Create a middleware
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
+    # request의 수행시간 측정
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
+    # custom proprietary header에 넣어서 client(user)에게 전달
     response.headers["X-Process-Time"] = str(process_time)
     return response
